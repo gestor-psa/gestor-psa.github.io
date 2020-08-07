8587def52bdc2c729fffb6e360eb2a52b9be3b03
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-07 17:50:31 +0000

    Deploy gestor-psa/gestor-psa.github.io to github.com/gestor-psa/gestor-psa.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.022d863418584194786da23c28eafded.js"
+  "/precache-manifest.bfc6aebd1dbab5949576aee0c44a200f.js"
 );
 
 self.addEventListener('message', (event) => {
